146f6680a9993e621df5989802372397ab0b60e1
innocent <geesnell.nich45@gmail.com>
2023-03-04 13:44:58 +0300

    week changele1

---
+++ user_input.py
@@ -0,0 +1,8 @@
+
+#Write a program that asks the user for their name, age, 
+# And location and then prints out a personalized message.
+
+name=input('Enter your name..:')
+age=input('Enter your age...:')
+location=input('Enter your location...:')
+print('hello',name,'yours',age,'years old','your location is at',location)
