634fab6b5ff58d6f0e9732f38b8dfcc8e2bd23d1
innocent <geesnell.nich45@gmail.com>
2023-03-05 15:25:18 +0300

    updation

--- user_input.py
+++ user_input.py
@@ -1,6 +1,9 @@
 
 #Write a program that asks the user for their name, age, 
 # And location and then prints out a personalized message.
+print('--------------------------------------')
+print('A PROGRAM THAT PRINT A PERSON MASSAGE' )
+print('--------------------------------------')
 
 name=input('Enter your name..')
 age=input('Enter your age...')
