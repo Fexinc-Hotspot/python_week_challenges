afbebfc08eb76114a514dc363c7bec954a47e068
innocent <geesnell.nich45@gmail.com>
2023-03-06 22:37:40 +0300

    First commit

--- set.py
+++ set.py
@@ -1,7 +1,20 @@
-print('-----------------------------------')
-print(' A PROGRAM TO STORE VALUES INTO SET')
-print('-----------------------------------')
-x = input('Enter two different fruits or more  with a comma : ')
+#print('-------------------------------------------------------------')
+#print(' A PROGRAM TO ACCEPT VALUES & DIPLAY TO THE USER BY USING  SET')
+#print('-------------------------------------------------------------')
+#x = input('Enter two different fruits or more  with a comma : ')
 
-x = {a for a in x.split(",")}
-print(x)
+#x = {a for a in x.split(",")}
+#print(x)
+
+#A PROGRAM THAT ACCEPT USERS INPUT TO CREATE TWO SET OF INTEGERS
+print('-----------------------------------------------------------------------------------')
+print('A PROGRAM TO PRINT TWO DIFFERENT INTEGERS SET & PRINT TWO ELEMENT WHICH ARE COMMON ')
+print('-----------------------------------------------------------------------------------')
+print()
+set_val=input('Enter Different integers value with a space:').split()
+print()
+set_x=set()
+set_x=set(set_val)
+print(set_x)
+print()
+
